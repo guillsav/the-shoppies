34ffead78d97f2a8149420949f3037ca07eaadbd
guillsav <guillsav@gmail.com>
2020-09-03 01:58:48 -0400

    fixed UI alignment

--- src/components/nominatedList/NominatedList.styles.js
+++ src/components/nominatedList/NominatedList.styles.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 
 export const NominatedContainer = styled.div`
   width: 100%;
-  height: 52.5rem;
+  height: 55.5rem;
   background: #15161a;
   border-radius: 0.2rem;
-  padding: 3rem;
+  padding: 6rem;
   display: flex;
   flex-direction: column;
   margin-bottom: 6rem;
@@ -31,7 +31,7 @@ export const SelectedList = styled.div`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(5, 1fr);
-  grid-gap: 2rem;
+  grid-gap: 3rem;
   margin-bottom: 3rem;
   transition: all 200ms ease-in-out;
 
